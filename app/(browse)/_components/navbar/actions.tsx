1175f46c4a6a1f5d04f0de082847d24a4e46598f
Juan David <juandavid918@gmail.com>
2024-07-31 15:39:26 -0500

    Navbar finalizado

---
+++ app/(browse)/_components/navbar/actions.tsx
@@ -0,0 +1,46 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Clapperboard } from "lucide-react";
+
+export const Actions = async() => {
+    const user = await currentUser();
+
+    return (
+        <div className="flex items-center justify-end gap-x-2 ml-4 lg:ml-0">
+            { !user && (
+                <SignInButton>
+                    <Button
+                        size= "sm"
+                        variant="primary"
+                    >
+                        Login
+                    </Button>
+                </SignInButton>
+            ) }
+
+            { !!user && (
+                <div className="flex items-center gap-x-4">
+                    <Button
+                        asChild
+                        className="text-muted-foreground hover:text-primary"
+                        size="sm"
+                        variant="ghost"
+                    >
+                        <Link href={ `/u/${ user.username }` }>
+                            <Clapperboard
+                                className="h-5 w-5 lg:mr-2"
+                            />
+
+                            <span className="hidden lg:block">Dashboard</span>
+                        </Link>
+                    </Button>
+
+                    <UserButton afterSignOutUrl="/" />
+                </div>
+            ) }
+        </div>
+    );
+}
---
+++ app/(browse)/_components/navbar/search.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { SearchIcon, X } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import qs from "query-string";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export const Search = () => {
+    const router = useRouter();
+
+    const [value, setValue] = useState("");
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if(!value) return;
+
+        const url = qs.stringifyUrl({
+            url: "/search ",
+            query: { term: value }
+        }, { skipEmptyString: true });
+
+        router.push(url);
+    }
+
+    const onClear = () => {
+        setValue("");
+    }
+
+    return (
+        <form
+        className="relative w-full lg:w-[400px] flex items-center"
+            onSubmit={ onSubmit }
+        >
+            <Input
+                className="rounded-r-none focus-visible:ring-0 focus-visible: ring-transparent focus-visible:ring-offset-0"
+                onChange={ (e) => setValue(e.target.value) }
+                placeholder="Search"
+                value={ value }
+            />
+
+            { value && (
+                <X
+                    className="absolute top-2.5 right-14 h-5 w-5 text-muted-foreground cursor-pointer hover:opacity-75 transition"
+                    onClick={ onClear }
+                />
+            ) }
+
+            <Button
+                className="rounded-l-none"
+                size="sm"
+                type="submit"
+                variant="secondary"
+            >
+                <SearchIcon className="h-5 w-5 text-muted-foregound" />
+            </Button>
+        </form>
+    );
+}
