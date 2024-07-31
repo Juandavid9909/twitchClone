4fc2663d85e261a88bf0053541ec7e0264f6179b
Juan David <juandavid918@gmail.com>
2024-07-31 13:46:10 -0500

    Conexión con la base de datos y clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
-import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+
+import { ThemeProvider } from "@/components/theme-provider";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +20,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <html lang="en">
+        <body className={ inter.className }>
+          <ThemeProvider
+            attribute="class"
+            forcedTheme="dark"
+            storageKey="gamehub-theme"
+          >
+            { children }
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
