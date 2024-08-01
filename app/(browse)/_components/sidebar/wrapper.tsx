217558515da2d320e42eca14d52f02f16ce11448
Juan David <juandavid918@gmail.com>
2024-08-01 11:54:16 -0500

    Follow list and recommended list updates

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -3,6 +3,7 @@
 import { useIsClient } from "usehooks-ts";
 
 import { cn } from "@/lib/utils";
+import { FollowingSkeleton } from "./following";
 import { RecommendedSkeleton } from "./recommended";
 import { ToggleSkeleton } from "./toggle";
 import { useSidebar } from "@/store/use-sidebar";
@@ -20,6 +21,8 @@ export const Wrapper = ({ children }: WrapperProps) => {
         return (
             <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-60">
                 <ToggleSkeleton />
+
+                <FollowingSkeleton />
     
                 <RecommendedSkeleton />
             </aside>
