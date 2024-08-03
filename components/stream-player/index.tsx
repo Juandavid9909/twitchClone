0c9514e8661db2063838c9e82ce1c81a254d790e
Juan David <juandavid918@gmail.com>
2024-08-03 12:55:03 -0500

    Chat component completed

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -3,8 +3,12 @@
 import { LiveKitRoom } from "@livekit/components-react";
 import { Stream, User } from "@prisma/client";
 
+import { Chat } from "./chat";
+import { cn } from "@/lib/utils";
+import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { useViewerToken } from "@/hooks/use-viewer-token";
 import { Video } from "./video";
+import { ChatToggle } from './chat-toggle';
 
 interface StreamPlayerProps {
     user: User & {
@@ -16,6 +20,7 @@ interface StreamPlayerProps {
 
 export const StreamPlayer = ({ user, stream, isFollowing }: StreamPlayerProps) => {
     const { token, name, identity } = useViewerToken(user.id);
+    const { collapsed } = useChatSidebar((state) => state);
 
     if(!token || !name || !identity) {
         return (
@@ -27,8 +32,17 @@ export const StreamPlayer = ({ user, stream, isFollowing }: StreamPlayerProps) =
 
     return (
         <>
+            { collapsed && (
+                <div className="hidden lg:block fixed top-[100px] right-2 z-50">
+                    <ChatToggle />
+                </div>
+            ) }
+
             <LiveKitRoom
-                className="grid grid-cols-1 lg:gap-y-0 lg:grip-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+                className={ cn(
+                    "grid grid-cols-1 lg:gap-y-0 lg:grip-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
+                    collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
+                ) }
                 serverUrl={ process.env.NEXT_PUBLIC_LIVEKIT_WS_URL }
                 token={ token }
             >
@@ -36,6 +50,23 @@ export const StreamPlayer = ({ user, stream, isFollowing }: StreamPlayerProps) =
                     <Video
                         hostname={ user.username }
                         hostIdentity={ user.id }
+                    />
+                </div>
+
+                <div
+                    className={ cn(
+                        "col-span-1",
+                        collapsed && "hidden"
+                    ) }
+                >
+                    <Chat
+                        viewerName={ name }
+                        hostName={ user.username }
+                        hostIdentity={ user.id }
+                        isFollowing={ isFollowing }
+                        isChatEnabled={ stream.isChatEnabled }
+                        isChatDelayed={ stream.isChatDelayed }
+                        isChatFollowersOnly={ stream.isChatFollowersOnly }
                     />
                 </div>
             </LiveKitRoom>
