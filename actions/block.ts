bab26bd7251b054518e8678d3c3e349654357131
Juan David <juandavid918@gmail.com>
2024-08-03 22:36:27 -0500

    User page and streaming video for users and guests

--- actions/block.ts
+++ actions/block.ts
@@ -1,19 +1,38 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { RoomServiceClient } from "livekit-server-sdk";
 
 import { blockUser, unblockUser } from "@/lib/block-service";
+import { getSelf } from "@/lib/auth-service";
+
+const roomService = new RoomServiceClient(
+    process.env.LIVEKIT_API_URL!,
+    process.env.LIVEKIT_API_KEY!,
+    process.env.LIVEKIT_API_SECRET!
+);
+
 
 export const onBlock = async (id: string) => {
-    // TODO: Adapt to disconnect from livestream
-    // TODO: Allow ability to kick the guest
-    const blockedUser = await blockUser(id);
+    const self = await getSelf();
 
-    revalidatePath("/");
+    let blockedUser;
 
-    if(blockedUser) {
-        revalidatePath(`/${ blockedUser.blocked.username }`);
+    try {
+        blockedUser = await blockUser(id);
     }
+    catch (error) {
+        // User is a guest
+    }
+
+    try {
+        await roomService.removeParticipant(self.id, id);
+    }
+    catch (error) {
+        // User is not in the room
+    }
+
+    revalidatePath(`/u/${ self.username }/community`);
 
     return blockedUser;
 }
